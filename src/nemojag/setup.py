839662a07983e8855790c8ffdcdbcfdfa90957c5
snorklerjoe <snorklerjoe@gmail.com>
2022-03-24 21:46:46 -0400

    Added some basic directory structure, etc.

---
+++ src/nemojag/setup.py
@@ -0,0 +1,10 @@
+'''
+    This script will by run by PIP when installing our package to the raspi.
+    It should set up any dependencies, etc. and still needs to be completed as of now.
+'''
+
+import setuptools
+
+#setuptools.setup(
+#     name='nemojag',
+#   ...
